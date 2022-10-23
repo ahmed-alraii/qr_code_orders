82c3b97d0413f95778b2e5c26ded53c2a04d83f1
ahmed-alameer <alameer8388@gmail.com>
2022-10-23 17:52:31 +0400

    enable update and delete menuItem

--- app/Http/Controllers/MenuItemController.php
+++ app/Http/Controllers/MenuItemController.php
@@ -11,81 +11,99 @@ use Illuminate\Support\Facades\Storage;
 
 class MenuItemController extends Controller
 {
+    private string $path = 'public/images/menu_items/';
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function index()
     {
         $records = MenuItem::all();
 
-        if(auth()->user()->role->name === 'Employee'){
-           $restaurant = $this->getEmployeeRestaurant()->first();
-            $records = MenuItem::where('restaurant_id' , $restaurant->id)->get();
+        if (auth()->user()->role->name === 'Employee') {
+            $restaurant = $this->getEmployeeRestaurant()->first();
+            $records = MenuItem::where('restaurant_id', $restaurant->id)->get();
         }
 
-        return view('menu_item.index' , compact('records' ));
+        return view('menu_item.index', compact('records'));
+    }
+
+    private function getEmployeeRestaurant()
+    {
+        return Restaurant::with('users')
+            ->whereHas('users', function ($query) {
+                $query->where('user_id', auth()->id());
+            })->get();
     }
 
     public function indexCards(Request $request)
     {
         $restaurant_id = $request->input('restaurant_id');
         $restaurant_name = $request->input('restaurant_name');
-        $records = MenuItem::where('restaurant_id' , $restaurant_id)->get();
+        $records = MenuItem::where('restaurant_id', $restaurant_id)->get();
         $cart_count = Cart::content()->count();
         $cart = Cart::content();
         $total = 0;
-        foreach ($cart as $item){
+        foreach ($cart as $item) {
             $total += $item->price;
         }
-        return view('menu_item.index_cards' , compact( 'records' ,'cart_count' , 'cart' , 'total' , 'restaurant_name'));
+        return view('menu_item.index_cards', compact('records', 'cart_count', 'cart', 'total', 'restaurant_name'));
     }
-
 
     public function create()
     {
         $restaurants = $this->getRestaurants();
-        return view('menu_item.create' , compact('restaurants'));
+        return view('menu_item.create', compact('restaurants'));
     }
 
-    private function getEmployeeRestaurant() {
-      return Restaurant::with('users')
-            ->whereHas('users', function($query) {
-                $query->where('user_id', auth()->id() );
-            })->get();
+    public function getRestaurants()
+    {
+        $restaurants = Restaurant::all();
+        if (auth()->user()->role->name === 'Employee') {
+            $restaurants = $this->getEmployeeRestaurant();
+        }
+        return $restaurants;
     }
-
 
     public function store(Request $request)
     {
         $data = $request->all();
-        $image = $request->file('image');
-        $folder = 'public/menu_items';
-        if($image !== null) {
+        $menuItem = MenuItem::create($data);
+        $image_name = $this->storeImage($request , $menuItem);
+        $menuItem->image = $image_name;
+        $menuItem->save();
+        return redirect()->route('menu_items.index');
+    }
+
+    public function storeImage(Request $request , $menuItem){
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
             $ext = $image->getClientOriginalExtension();
             $validExtensions = ['png', 'jpg', 'jpeg'];
 
-            if ($image->isFile()) {
-                if (!in_array($ext, $validExtensions)) {
-                    Session::flash('message', 'Image Not Valid.');
-                    Session::flash('alert-class', 'alert-danger');
-                    return redirect()->back()->withInput();
-                }
-                $image_url = Storage::putFile($folder, $image);
-                $image_url_array = explode('/' , $image_url);
-                $image_name = $image_url_array[2];
-                $data['image'] = $image_name;
+            if (!in_array($ext, $validExtensions)) {
+                Session::flash('message', 'Image Not Valid.');
+                Session::flash('alert-class', 'alert-danger');
+                return redirect()->back()->withInput();
             }
+
+            if($request->method() === 'PUT'){
+                if($menuItem->image !== null){
+                    Storage::delete( $this->path . $menuItem->image);
+                }
+            }
+
+            $image_name = $menuItem->id. '.' . $ext;
+            $image->storeAs( $this->path, $image_name);
+
+
+        } else {
+            $image_name = $menuItem->image ?? 'default_img.png';
         }
-
-        if($image === null){
-            $data['image'] = $folder . 'default_img.jpg';
-        }
-
-        $res =  MenuItem::create($data);
-
-        if($res){
-            return redirect()->route('menu_items.index');
-        }
-
-        return redirect()->back()->withInput();
+        return $image_name;
     }
 
     public function show(MenuItem $menuItem)
@@ -93,42 +111,30 @@ class MenuItemController extends Controller
         //
     }
 
-
     public function edit(MenuItem $menuItem)
     {
         $restaurants = $this->getRestaurants();
-        return view('menu_item.edit' , ['record' => $menuItem , 'restaurants' => $restaurants]);
+        return view('menu_item.edit', ['record' => $menuItem, 'restaurants' => $restaurants]);
     }
 
-    public function getRestaurants()
+    public function update(Request $request, MenuItem $menuItem)
     {
-        $restaurants = Restaurant::all();
-        if(auth()->user()->role->name === 'Employee'){
-            $restaurants = $this->getEmployeeRestaurant();
+        $data = $request->all();
+        $image_name = $this->storeImage($request , $menuItem);
+        $data['image'] = $image_name;
+        $menuItem->update($data);
+        return redirect()->route('menu_items.index');
+    }
+
+
+    public function destroy(MenuItem $menuItem)
+    {
+
+        if( $menuItem->image !== 'default_img.png'){
+            Storage::delete($this->path . $menuItem->image);
         }
-        return $restaurants;
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\MenuItem  $munuItem
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, MenuItem $munuItem)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\MenuItem  $munuItem
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(MenuItem $munuItem)
-    {
-        //
+        $menuItem->delete();
+        return redirect()->route('menu_items.index');
     }
 }
